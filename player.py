4d7da0c68e2f00ee2ff913ef3a44c1a1444cb666
karvounakos <119814467+karvounakos@users.noreply.github.com>
2024-10-31 22:21:53 +0200

    Add files via upload

---
+++ player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self, max_depth, player_letter):
+        self._maxDepth = max_depth
+        self.player_letter = player_letter
+    
+    def mini_max(self, board):
+        return None
+    
+    def max(self, board, depth):
+        return None
+    
+    def min(self, board, depth):
+        return None
